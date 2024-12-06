af33f012f59425d1882f9758022552a80b852ae5
VITOR PAES RODRIGUES <24122327@CFP127ALUNO.LOCAL>
2024-12-06 11:23:40 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,199 @@
+/*******************************************************************************************************************************************************
+ * Objetivo: API para manipular dados de cursos e alunos.
+ * Data: 22/11/24
+ * Autor: Vitor Paes Rodrigues
+ * Versão: 1.0
+*******************************************************************************************************************************************************/
+
+/**
+ * Para Criar uma API devemos instalar -->
+ * 
+ * express        npm install  express --save            --Serve para criar a API
+ * cors           npm install  cors --save               --Serve para configurar as permissões da API
+ * body-parser    npm install  body-parser --save        --Serve para manipular os dados do body da API
+*/
+
+const  express = require('express');
+const  cors = require('cors');
+const  bodyParser = require('body-parser');
+
+//Inicializanddo o express através do objeto app
+const app = express()
+
+//request   -->    Dados que chegam na API
+//response  -->    Dados que a API envia de volta para o cliente
+app.use((request,response, next)=>{
+    //Permissão de acesso para liberar quais máquinas poderão usar a API
+    response.header('Acess-Control-Allow-Origin',  '*');
+    //Permissão de acesso para liberar os verbos da requisição da API
+    response.header('Acess-Control-Allow-Methods',  'GET');
+
+    app.use(cors())//Ativando as configurações do cors
+
+    next()
+})
+
+const alunosCursos = require('./modulo/funcoes.js')
+
+//Recupera uma lista de todos os cursos oferecidos pela escola. 
+app.get('/v1/lion-school/lista-cursos', cors(), async function(request, response){
+    let listaCursos = alunosCursos.getListaDeCursos()
+
+    if(listaCursos){
+        response.status(200) //sucess
+        response.json(listaCursos)
+    }else{
+        response.status(404) // not found
+        response.json({'status': 404, 'message':  'Não foi possível encontrar nenhum item de retorno.'})
+    }
+})
+
+//Recupera uma lista de todos os alunos matriculados na escola.
+app.get('/v1/lion-school/lista-alunos', cors(), async function(request, response){
+    let listaAlunos = alunosCursos.getListaAlunos()
+
+    if(listaAlunos){
+        response.status(200) //sucess
+        response.json(listaAlunos)
+    }else{
+        response.status(404) // not found
+        response.json({'status': 404, 'message':  'Não foi possível encontrar nenhum item de retorno.'})
+    }
+})
+
+//Recupera informações de um aluno específico com base no número de matrícula. 
+app.get('/v1/lion-school/lista-alunos/:matricula', cors(), async function(request, response){
+    let matriculaDados = request.query.matricula
+
+    let alunoMatricula = alunosCursos.matriculaAluno(matriculaDados)
+
+    if(alunoMatricula){
+        response.status(200)
+        response.json(alunoMatricula)
+    }else{
+        response.status(404)
+        response.json({'status': 404, 'message': 'Matrícula não localizado.'})
+    }
+})
+
+//Recupera uma lista de todos os alunos matriculados no curso especificado. DS ou REDES 
+app.get('/v1/lion-school/alunos-matriculados/:curso', cors(), async function(request, response){
+    let dados = request.params.curso
+
+    let alunosDoCurso = alunosCursos.alunoCurso(dados)
+
+    if(alunosDoCurso){
+        response.status(200)
+        response.json(alunosDoCurso)
+    }else{
+        response.status(404)
+        response.json({'status': 404, 'message': 'Nenhum aluno foi localizado.'})
+    }
+})
+
+//Recupera uma lista de todos os alunos com o status especificado. Finalizado ou Cursando
+app.get('/v1/lion-school/alunos/filtro', cors(), async function(request, response){
+    let dados = request.query.status
+
+    let alunosDoCurso = alunosCursos.alunoStatus(dados)
+
+    if(alunosDoCurso){
+        response.status(200)
+        response.json(alunosDoCurso)
+    }else{
+        response.status(404)
+        response.json({'status': 404, 'message': 'Nenhum aluno foi localizado.'})
+    }
+})
+
+app.get('/v1/lion-school/alunos/filtro', cors(), async function(request, response){
+    let curso = request.query.curso
+    let status = request.query.status
+
+
+    let alunosDoCurso = alunosCursos.disciplinaStatus(nomeCurso, dados)
+
+    if(alunosDoCurso){
+        response.status(200)
+        response.json(alunosDoCurso)
+    }else{
+        response.status(404)
+        response.json({'status': 404, 'message': 'Nenhum aluno foi localizado.'})
+    }
+})
+
+
+app.get('/v1/lion-school/alunos/filtro', cors(), async function(request, response){
+    let dados = request.query.curso
+
+    let alunosDoCurso = alunosCursos.alunoCurso(dados)
+
+    if(alunosDoCurso){
+        response.status(200)
+        response.json(alunosDoCurso)
+    }else{
+        response.status(404)
+        response.json({'status': 404, 'message': 'Nenhum aluno foi localizado.'})
+    }
+})
+
+app.get('/v1/estados-cidades/capital/estado', cors(), async function(request, response){
+    let uf = request.query.sigla
+
+    let dados = estadosCidades.getCapitalEstado(uf)
+    if(dados){
+        response.status(200)
+        response.json(dados)
+    }else{
+        response.status(404)
+        response.json({'status': 404, 'message': 'Não foi possível encontrar os dados dos estados.'})
+    }
+
+
+    console.log(uf);
+})
+
+app.get('/v1/estados-cidades/estados/regiao/', cors(), async function(request, response){
+    let uf = request.query.regiao
+
+    let dadosRegiao = estadosCidades.getEstadosRegiao(uf)
+    if(dadosRegiao){
+        response.status(200)
+        response.json(dadosRegiao)
+    }else{
+        response.status(404)
+        response.json({'status': 404, 'message': 'Não foi possível encontrar os dados dos estados.'})
+    }
+})
+
+app.get('/v1/estados-cidades/estados/capitais/pais', cors(), async function(request, response){
+    let dadosCapitais = estadosCidades.getCapitalPais()
+
+    if(dadosCapitais){
+        response.status(200)
+        response.json(dadosCapitais)
+    }else{
+        response.status(404)
+        response.json({'status': 404, 'message': 'Não foi possível encontrar os dados dos estados.'})
+    }
+})
+
+app.get('/v1/estados-cidades/estados/cidades', cors(), async function(request, response){
+    //Recebe o parâmetro(variável) :sigla pera URL
+    let uf = request.query.sigla
+
+    //Chama a funçao para filtrar o estado
+    let dados = estadosCidades.getCidades(uf)
+
+    if(dados){
+        response.status(200)
+        response.json(dados)
+    }else{
+        response.status(404)
+        response.json({'status': 404, 'message': 'Estados não localizado.'})
+    }
+})
+
+app.listen('8080', function(){
+    console.log('API aguardando requisições...');
+})
---
+++ modulo/funcoes.js
@@ -0,0 +1,211 @@
+/*******************************************************************************************************************************************************
+ * Objetivo: Criar uma aplicação Back-End para monitorar dados dos alunos da escola Lion.
+ * Data: 22/11/24
+ * Autor: Vitor Paes
+ * Versão: 1.0
+*******************************************************************************************************************************************************/
+var cursoss = require('./cursos.js')
+var alunoss = require('./alunos.js')
+var listaCursos = cursoss.cursos
+var listaAlunos = alunoss.alunos
+
+
+//var listaEstados = lista.listaDeEstados.estados
+
+
+/*****************************************************************************************
+ *Criar uma função que retorna a lista de todos os cursos registrados.
+ *****************************************************************************************/
+const getListaDeCursos = function(){
+    let retorno = {quantidade: 0, curso: []}
+    let quantidade = 0
+    
+    listaCursos.forEach(function(item){
+        retorno.curso.push(item)
+        quantidade++
+    })
+    retorno.quantidade = quantidade
+
+    if(quantidade > 1){
+            return retorno
+    }else{
+        return false
+    } 
+}
+//console.log(getListaDeCursos())
+
+/*****************************************************************************************
+ *Criar uma função que retorna a lista de todos os alunos matriculados.
+ *****************************************************************************************/
+const getListaAlunos = function(){
+    let retorno = {quantidade: 0, alunos: []}
+    let quantidade = 0
+    
+    listaAlunos.forEach(function(item){
+        retorno.alunos.push(item)
+        quantidade++
+    })
+    retorno.quantidade = quantidade
+
+    if(quantidade > 1){
+            return retorno
+    }else{
+        return false
+    } 
+}
+//console.log(getListaAlunos())
+
+/**********************************************************************************************
+ *Criar uma função que retorna um aluno específico, onde a matrícula será o critério de filtro. 
+ *********************************************************************************************/
+const matriculaAluno = function(valor){
+    let matricula = valor
+    let informar =  {aluno: []}
+
+    listaAlunos.forEach(function(item){
+        if(item.matricula == matricula){
+            informar.aluno.push(item)
+        }
+    })
+    if(matricula == ''){
+        return false
+    }else{
+        return  informar
+    }
+}
+//console.log(matriculaAluno('20151001019'));
+
+
+/**************************************************************************
+*Criar uma função que lista todos alunos matriculados no curso especificado.
+**************************************************************************/
+const alunoCurso = function(nome){
+    let curso = String(nome.toUpperCase())
+    let  informar = {curso: curso, alunos: []}
+
+    listaAlunos.forEach(function(item){
+        item.curso.forEach(function(nomeCurso){
+
+            if(nomeCurso.sigla == curso){
+                informar.alunos.push(item)
+            }
+        })
+    })
+    if(curso == '' || informar.alunos.length < 1){
+        return false
+    }else{
+        return  informar
+    }
+}
+//console.log(alunoCurso('ds'));
+
+/*************************************************************************************
+*Criar uma função que lista todos alunos com o status de 'Cursando' ou 'Finalizado'.
+*************************************************************************************/
+const alunoStatus = function(situacao){
+    let status = situacao
+    let informar = {status: status, alunos: []}
+
+    listaAlunos.forEach(function(item){
+        if (item.status == status){ 
+            informar.alunos.push(item)
+        }
+    })
+
+    if (status === '' || informar.alunos.length < 1){
+        return false
+    } else {
+        return informar
+    }
+}
+
+//console.log(alunoStatus('Finalizado'))
+
+/**********************************************************************************************************************
+*Criar uma função que lista todos alunos matriculados no curso especificado e com base em um status de uma disciplina.*
+**********************************************************************************************************************/
+const disciplinaStatus = function(nomeCurso, situacao){
+    let curso = String(nomeCurso.toUpperCase())
+    let status = situacao
+    let  informar = {curso: curso, status: status, alunos: []}
+
+    listaAlunos.forEach(function(item){
+
+        item.curso.forEach(function(cursoNome){
+
+            if(cursoNome.sigla == curso){
+                let statusDisciplinas = []
+                
+                cursoNome.disciplinas.forEach(function(disciplinaCurso){
+                    if(disciplinaCurso.status == status){
+                        statusDisciplinas.push(disciplinaCurso)
+                    }
+                })
+                if (statusDisciplinas.length > 0) {
+                    informar.alunos.push(item)
+                }
+            }
+        })
+    })
+    if(curso == '' || status == '' || informar.alunos.length < 1){
+        return false
+    }else{
+         return  informar
+    }
+}
+//console.log(disciplinaStatus('DS', 'Exame'));
+
+
+/**************************************************************************************************************
+*Criar uma função que lista todos alunos matriculados em um curso especificado e com base no ano de conclusão.*
+**************************************************************************************************************/
+const cursoAnoConclusao = function(nomeCurso, data){
+    let curso = String(nomeCurso.toUpperCase())
+    let anoConclusao = data
+    let  informar = {curso: curso, anoDeConclusao: anoConclusao, alunos: []}
+
+    listaAlunos.forEach(function(item){
+
+        item.curso.forEach(function(cursoEscolhido){
+
+            if(cursoEscolhido.sigla == curso && cursoEscolhido.conclusao == anoConclusao){
+                informar.alunos.push(item)
+            }
+        })
+    })
+
+    if(curso == '' || anoConclusao == ''){
+        return false
+    }else if(informar.alunos.length < 1){
+        console.log('Nenhum aluno se formou no ano inserido.');
+        return false
+    }else{
+        return  informar
+    }
+}
+//console.log(cursoAnoConclusao('ds', '2023'))
+
+const tresFuncoes = function (dado1, dado2) {
+    let valor1 = dado1
+    let valor2 = dado2
+    if(valor1 && valor2 === ''){
+        return alunoStatus
+    }else if(valor1 && valor2){
+        if(!isNaN(valor1) || !isNaN(valor2)){
+            return cursoAnoConclusao
+        }
+        else{
+            return disciplinaStatus
+        }
+    }
+}
+
+module.exports = {
+    getListaDeCursos,
+    getListaAlunos,
+    matriculaAluno,
+    alunoCurso,
+    alunoStatus,
+    disciplinaStatus,
+    cursoAnoConclusao
+}
